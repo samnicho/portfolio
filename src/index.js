a67214e8af13532e5b620ec1d9b36ca68584b085
Sam Nicholson <samnicho@Sams-MacBook-Pro.local>
2020-09-24 17:01:13 +0100

    removed PORT=5000 from dev.env

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const app = require('./app'); // require the express server
 
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
