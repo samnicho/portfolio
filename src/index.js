8bb9e050b2245722f44aacb81fa9914c016c2c46
Sam Nicholson <samnicho@Sams-MacBook-Pro.local>
2020-09-25 12:49:25 +0100

    changed index.js: express.json to express.static

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express'); //require express
 require('../db/mongoose'); // require the mongoose.js file (automatically connects to the db)
 const projectRouter = require('../routers/project'); 
@@ -5,16 +6,18 @@ const connectDetailsRouter = require('../routers/connectDetails');
 const pageContentRouter = require('../routers/pageContent');
 
 const app = express();
+const port = process.env.PORT || 5000;
+const publicDirectoryPath = path.join(__dirname, '../client/public')
+
+app.use(express.static(publicDirectoryPath))
 
 // configure express to parse results as JSON
-app.use(express.json());
+// app.use(express.json());
 
 // tell Express to use the imported Routers
 app.use(projectRouter);
 app.use(connectDetailsRouter);
 app.use(pageContentRouter);
-
-const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
