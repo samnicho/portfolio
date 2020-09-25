724bdc93da57b6c3357949e44daee4e3f2284150
Sam Nicholson <samnicho@Sams-MacBook-Pro.local>
2020-09-25 11:03:29 +0100

    dev.env changes

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,6 +1,15 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URL, {
+// mongoose.connect(process.env.MONGODB_URL, {
+//     useNewUrlParser: true,
+//     useCreateIndex: true,
+//     useUnifiedTopology: true,
+//     useFindAndModify: false
+// }, () => {
+//     console.log('mongoose connected');
+// });
+
+mongoose.connect('mongodb+srv://samnicho-portfolio:yQUcqG3rgWEtdInB@cluster0-nzjwi.mongodb.net/test?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const app = require('./app'); // require the express server
 
-const port = process.env.PORT || 5000;
+// const port = process.env.PORT;
+
+const port = process.env.PORT || process.env.MY_PORT || 5000;
 
 app.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
