d618ce324e1f7b42c479b346e710cd57f7cc67a7
Sam Nicholson <samnicho@Sams-MacBook-Pro.local>
2020-09-25 11:40:36 +0100

    moved app.js into index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
-const app = require('./app.js'); // require the express server
+const express = require('express'); //require express
+require('../db/mongoose'); // require the mongoose.js file (automatically connects to the db)
+const projectRouter = require('../routers/project'); 
+const connectDetailsRouter = require('../routers/connectDetails');
+const pageContentRouter = require('../routers/pageContent');
 
-const port = process.env.PORT;
+const app = express();
+
+// configure express to parse results as JSON
+app.use(express.json());
+
+// tell Express to use the imported Routers
+app.use(projectRouter);
+app.use(connectDetailsRouter);
+app.use(pageContentRouter);
+
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
